1c2ad299d62bbc2252d4dfd10471ff78602c5657
NeerajFull <117462156+NeerajFull@users.noreply.github.com>
2025-05-04 22:00:02 +0530

    fix: added correct environment name

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" ? "https://commurax.onrender.com/" : "http://localhost:5173",
+    origin: process.env.MODE === "production" ? "https://commurax.onrender.com/" : "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
