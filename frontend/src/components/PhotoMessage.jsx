78012d0d18f74fe2173af78bcebcf510be8c2f87
NeerajFull <117462156+NeerajFull@users.noreply.github.com>
2025-05-20 00:04:44 +0530

    fix: added preview image functionality

--- frontend/src/components/PhotoMessage.jsx
+++ frontend/src/components/PhotoMessage.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 
 const PhotoMessage = ({ classes, photoUrl }) => {
+    const [preview, setPreview] = useState(false);
+    const handlePreview = () => {
+        setPreview(true);
+    }
+
     return (
-        <img src={photoUrl} alt="message-photo" className={`max-w-60 max-h-72 ${classes}`} />
+        <div className="relative">
+            <img src={photoUrl} alt="message-photo" className={`max-w-60 max-h-72 ${classes}`} onClick={handlePreview} />
+
+            {(preview &&
+                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex items-center justify-center z-50">
+                    <button className="absolute top-4 right-4 text-white text-4xl" onClick={() => setPreview(false)}>
+                        X
+                    </button>
+                    <img src={photoUrl} alt="preview-photo" className="max-w-full max-h-full object-contain" />
+                </div>
+            )}
+
+        </div>
+
     );
 }
 
